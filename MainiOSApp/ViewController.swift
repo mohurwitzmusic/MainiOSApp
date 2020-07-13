238a04bae03b0ff44ac15a8f59f72872dd10fca7
MIDIKinetics <moh@midikinetics.com>
2020-07-12 17:56:59 -0700

    Initial Commit

---
+++ MainiOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MainiOSApp
+//
+//  Created by Michael O. Hurwitz on 2020-07-12.
+//  Copyright © 2020 MIDI Kinetics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
